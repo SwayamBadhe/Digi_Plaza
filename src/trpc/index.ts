e57ea05ba51f54e3006b0e6eda6340b522b53d58
Swayam Badhe <swayampbadhe@gmail.com>
2024-06-06 01:08:29 +0530

    refactor: Update appRouter definition in trpc index

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,11 +1,10 @@
 // backend
 
-import { publicProcedure, router } from './trpc';
+import { authRouter } from './auth-router';
+import { router } from './trpc';
 
 export const appRouter = router({
-  anyApiRoute: publicProcedure.query(() => {
-    return 'hello';
-  }),
+  auth: authRouter,
 });
 
 export type AppRouter = typeof appRouter;
