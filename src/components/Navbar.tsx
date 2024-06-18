9fd2cdf84fdb6fa54ac01f331a75169ddad383cc
Swayam Badhe <swayampbadhe@gmail.com>
2024-06-18 20:11:15 +0530

    feat: integrate server-side user fetching in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,13 @@ import { Icons } from './Icons';
 import NavItems from './NavItems';
 import Cart from './Cart';
 import { buttonVariants } from './ui/button';
+import { getServerSideUser } from '@/lib/payload-utils';
+import { cookies } from 'next/headers';
+import UserAccountNav from './UserAccountNav';
 
-const Navbar = () => {
-  const user = null;
+const Navbar = async () => {
+  const nextCookies = cookies();
+  const { user } = await getServerSideUser(nextCookies);
 
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
@@ -35,7 +39,7 @@ const Navbar = () => {
                   )}
                   {user ? null : <span className="h-6 w-px bg-gray-200" />}
                   {user ? (
-                    <p></p>
+                    <UserAccountNav user={user} />
                   ) : (
                     <Link
                       href="/sign-up"
