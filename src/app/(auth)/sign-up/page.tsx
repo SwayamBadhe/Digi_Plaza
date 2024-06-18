0779f61f9096e524084c07afe20039d1fbccb070
Swayam Badhe <swayampbadhe@gmail.com>
2024-06-18 20:10:28 +0530

    feat: enhance user registration form with improved error handling

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -15,6 +15,8 @@ import {
   TAuthCredentialsValidator,
 } from '@/lib/validators/account-credentials';
 import { trpc } from '@/trpc/client';
+import { toast } from 'sonner';
+import { ZodError } from 'zod';
 
 const Page = () => {
   const {
@@ -25,7 +27,21 @@ const Page = () => {
     resolver: zodResolver(AuthCredentialsValidator),
   });
 
-  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({});
+  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
+    onError: (err) => {
+      if (err.data?.code === 'CONFLICT') {
+        toast.error('This email is already on use');
+        return;
+      }
+
+      if (err instanceof ZodError) {
+        toast.error(err.issues[0].message);
+        return;
+      }
+
+      toast.error('Something went wrong, please try again');
+    },
+  });
 
   const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
     // send data to server
@@ -34,7 +50,7 @@ const Page = () => {
 
   return (
     <>
-      <div className="container">
+      <div className="container relative flex flex-col items-center justify-center pt-20 lg:px-0">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col items-center space-y-2">
             <Icons.logo className="h-20 w-20" />
@@ -52,7 +68,7 @@ const Page = () => {
             </Link>
           </div>
 
-          <div className="grid">
+          <div className="grid gap-6">
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="grid gap-2">
                 <div className="grid gap-1 py-2">
@@ -60,10 +76,15 @@ const Page = () => {
                   <Input
                     {...register('email')}
                     className={cn({
-                      'focus-visible: ring-red-500': errors.email,
+                      'focus-visible:ring-red-500': errors.email,
                     })}
                     placeholder="you@example.com"
                   />
+                  {errors?.email && (
+                    <p className="text-sm text-red-500">
+                      {errors.email.message}
+                    </p>
+                  )}
                 </div>
                 <div className="grid gap-1 py-2">
                   <Label htmlFor="password">Password</Label>
@@ -71,10 +92,15 @@ const Page = () => {
                     {...register('password')}
                     type="password"
                     className={cn({
-                      'focus-visible: ring-red-500': errors.password,
+                      'focus-visible:ring-red-500': errors.password,
                     })}
                     placeholder="password"
                   />
+                  {errors?.password && (
+                    <p className="text-sm text-red-500">
+                      {errors.password?.message}
+                    </p>
+                  )}
                 </div>
                 <Button>Sign up</Button>
               </div>
